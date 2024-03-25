00ee937f02aad922c2a82c9d1f1c49e985248f61
Youssef Aboalyouser <youssefaboalyouser@gmail.com>
2024-03-25 02:37:43 +0200

    updated version of filters

--- assignment.cpp
+++ assignment.cpp
@@ -6,6 +6,39 @@ using namespace std;
 //ahmed ====>  2 filter(2,5,8,11)
 //mohamed ====>  2 filter(3,6,9,12)
 // =======================================================================================
+void darken_lighten_filter(Image& picture,string ver)
+{
+    if(ver == "i" || ver == "I")//darken version
+    {
+        for(int i = 0 ; i < picture.width ; i++)
+        {
+            for(int j = 0 ; j < picture.height ; j++)
+            {
+                picture(i,j,0) *= 0.5f;
+                picture(i,j,1) *= 0.5f;
+                picture(i,j,2) *= 0.5f;
+
+            }
+        }
+
+    }
+    else if(ver == "I" || ver == "II")//lighten version
+    {
+        for(int i = 0 ; i < picture.width ; i++)
+        {
+            for(int j = 0 ; j < picture.height ; j++)
+            {
+                picture(i,j,0) *= 1.5f;
+                picture(i,j,1) *= 1.5f;
+                picture(i,j,2) *= 1.5f;
+
+            }
+        }
+
+    }
+
+}
+
 
 void GrayscaleFilter(Image& picture)
 {
@@ -23,10 +56,28 @@ void GrayscaleFilter(Image& picture)
             picture(i,j,1) = average;
             picture(i,j,2) = average;
         }
-
     }
 }
-
+// black and white filter
+void black_and_whiteFilter(Image& photo){
+    // convert to black and white
+    unsigned int Midpoint = 127; // Midpoint of 255 grayscale levels
+    for (int i = 0; i < photo.width; ++i) {
+        for (int j = 0; j < photo.height; ++j) {
+            unsigned int Pixel = photo(i, j, 0); 
+            if (Pixel > Midpoint) {
+                for (int k = 0; k < 3; ++k) {
+                    photo(i, j, k) = 255; // Set to white
+                }
+            } else {
+                for (int k = 0; k < 3; ++k) {
+                    photo(i, j, k) = 60; // Set to black
+                    photo(i, j, k) = 0; // Set to black
+                }
+            }
+        }
+    }
+}
 int main()
 {
     while (true)
@@ -45,10 +96,10 @@ int main()
             Image picture(filename);
             cout << "***we have lots of filters to applay***" << endl;
             cout <<"=============================================================================\n";
-            cout << "A) Grayscale conversion                B) Blach and white\n"<<endl;
-            cout << "C)Invert image                         D) Merge images\n"<<endl;
+            cout << "A) Grayscale conversion                B) Black and white\n"<<endl;
+            cout << "C) Invert image                         D) Merge images\n"<<endl;
             cout << "E) Flip image                          F) Rotat image\n"<<endl;
-            cout << "G) Darken and Lighten image            H) Crop images\n"<<endl;
+            cout << "G) Darken or Lighten image             H) Crop images\n"<<endl;
             cout << "I) Adding a frame to the picture       J) Detect image edges\n"<<endl;
             cout << "K) Resizing images                     L) Blur images\n"<<endl;
             cout <<"=============================================================================\n";
@@ -64,79 +115,93 @@ int main()
                     cin >> filename;
                     GrayscaleFilter(picture);
                     picture.saveImage(filename);
-
                 }
                 else{
                     exit(EXIT_SUCCESS);
                 }
                 
-
             }
             else if(filterchooce == "B" || filterchooce == "b")
             {
-                // make function do the filter and call it here 
-                
-            }
+                cout << "do you want to save new image[Y/N]: ";
+                cin >> ask;
+                if(ask == "Y" || ask == "y")
+                {
+                    cout << "write your name of new image(don't forget extension): ";
+                    cin >> filename;
+                    GrayscaleFilter(picture);
+                    black_and_whiteFilter(picture);
+                    // black_and_whiteFilter(photo);
+                    picture.saveImage(filename);
 
+                }
+                else{
+                    exit(EXIT_SUCCESS);
+                }
+            }
             else if(filterchooce == "C" || filterchooce == "c")
             {
                 // make function do the filter and call it here 
-
             }
             else if(filterchooce == "D" || filterchooce == "d")
             {
-
                 // make function do the filter and call it here 
                 // make function do the filter and call it here 
             }
             else if(filterchooce == "E" || filterchooce == "e")
             {
-
                 // make function do the filter and call it here 
             }
             else if(filterchooce == "F" || filterchooce == "f")
             {
-
                 // make function do the filter and call it here 
             }
             else if(filterchooce == "G" || filterchooce == "g")
             {
+                cout << "do you want to save new image[Y/N]: ";
+                cin >> ask;
+                if(ask == "Y" || ask == "y")
+                {
+                    string ver;
+                    cout << "i)darken version         ii)lighten version " <<endl;
+                    cout <<": ";
 
-                // make function do the filter and call it here 
+                    cin >> ver;
+                    cout << "write your name of new image(don't forget extension): ";
+                    cin >> filename;
+                    darken_lighten_filter(picture,ver);
+                    picture.saveImage(filename);
+                }
+                else{
+                    exit(EXIT_SUCCESS);
+                }
+               
             }
             else if(filterchooce == "H" || filterchooce == "h")
             {
-
                 // make function do the filter and call it here 
             }
             else if(filterchooce == "I" || filterchooce == "i")
             {
-
                 // make function do the filter and call it here 
             }
             else if(filterchooce == "J" || filterchooce == "j")
             {
-
                 // make function do the filter and call it here 
-
             }
             else if(filterchooce == "K" || filterchooce == "k")
             {
                 // make function do the filter and call it here 
-
             }
             else if(filterchooce == "L" || filterchooce == "l")
             {
                 // make function do the filter and call it here 
-
             }
             else{
                 cout << "Error.try again!" <<endl;
                 break;
             }
-
         }
-
         else if(chooce == "N" || chooce == "n")
         {
             cout << "***GODE BYE***";
@@ -146,4 +211,3 @@ int main()
     
     
 }
-
