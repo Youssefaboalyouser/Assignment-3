306ade78a2db68beb39bf0f2cd16920eaba6f918
Youssef Aboalyouser <youssefaboalyouser@gmail.com>
2024-04-08 09:07:25 +0200

    all filters for youssef are finished <=

--- assignment.cpp
+++ assignment.cpp
@@ -25,6 +25,76 @@
 using namespace std;
 string filename;
 int main();
+void mergetowpic(Image &picture1, Image &picture2)
+{
+
+    string ask;
+    cout << "do you want to save new image[Y/N]: ";
+    cin >> ask;
+    if (ask == "Y" || ask == "y")
+    {
+        cout << "write your name of new image(don't forget extension): ";
+        cin >> filename;
+        int new_width = 1024;
+        int new_hight = 1024;
+        Image new_picture1(new_width, new_hight);
+        Image new_picture2(new_width, new_hight);
+        Image final_photo(new_width, new_hight);
+        float x_scale1 = static_cast<float>(picture1.width) / new_width;
+        float y_scale1 = static_cast<float>(picture1.height) / new_hight;
+
+        float x_scale2 = static_cast<float>(picture2.width) / new_width;
+        float y_scale2 = static_cast<float>(picture2.height) / new_hight;
+
+        for (int y = 0; y < new_hight; ++y)
+        {
+            for (int x = 0; x < new_width; ++x)
+            {
+                int origx1 = static_cast<int>(x * x_scale1);
+                int origy1 = static_cast<int>(y * y_scale1);
+
+                int origx2 = static_cast<int>(x * x_scale2);
+                int origy2 = static_cast<int>(y * y_scale2);
+
+                for (int c = 0; c < 3; ++c)
+                {
+                    new_picture1.imageData[(y * new_width + x) * 3 + c] =
+                        picture1.imageData[(origy1 * picture1.width + origx1) * 3 + c];
+
+                    new_picture2.imageData[(y * new_width + x) * 3 + c] =
+                        picture2.imageData[(origy2 * picture2.width + origx2) * 3 + c];
+                }
+            }
+        }
+        for (int i = 0; i < final_photo.width; i++)
+        {
+            for (int j = 0; j < final_photo.height; j++)
+            {
+                unsigned int picture1_pixel;
+                unsigned int picture2_pixel;
+                for (int k = 0; k < 3; k++)
+                {
+                    picture1_pixel = new_picture1(i, j, k);
+                    picture2_pixel = new_picture2(i, j, k);
+                }
+                final_photo(i, j, 0) = (new_picture1(i, j, 0) + new_picture2(i, j, 0)) / 2; // Red channel
+                final_photo(i, j, 1) = (new_picture1(i, j, 1) + new_picture2(i, j, 1)) / 2; // Green channel
+                final_photo(i, j, 2) = (new_picture1(i, j, 2) + new_picture2(i, j, 2)) / 2; // Blue channel
+            }
+        }
+        final_photo.saveImage(filename);
+    }
+    else if (ask == "N" || ask == "n")
+    {
+        main();
+    }
+    else
+    {
+        cout << "ERROR! , try again" << endl;
+        cout << "=========================================================================\n";
+        main();
+    }
+}
 
 void edgedetictiveFilter(Image &picture)
 {
@@ -339,9 +409,14 @@ int main()
             }
             else if (filterchooce == "D" || filterchooce == "d")
             {
-                cout << "not completed yet!" << endl;
-                cout << "==========================================================================\n";
-                main();
+                cout << "put your second image name or path (with extension): ";
+                cin >> filename;
+                Image picture2(filename);
+
+                mergetowpic(picture, picture2);
+                // cout << "not completed yet!" << endl;
+                // cout << "==========================================================================\n";
+                // main();
             }
             else if (filterchooce == "E" || filterchooce == "e")
             {
